d0408316311177e9d809c1168e88d33dab9b913c
projectusers12 <projectusers12@gmail.com>
2026-04-07 08:26:10 +0530

    Update stop1.py

--- stop1.py
+++ stop1.py
@@ -4,14 +4,14 @@ from nltk.corpus import stopwords
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
 
-# 1. Read the raw text from the first file
+
 with open('pre.txt', 'r') as file1:
-    words = file1.read().split() # .split() chops it into words using spaces
+    words = file1.read().split() 
 
 # 2. Filter the words and write them to the new file
 with open('to_write_data.txt', 'w') as file2:
     for w in words:
         if w.casefold() not in stop_words:
-            file2.write(w + " ") # Write the word and add a space after it
+            file2.write(w + " ")
             
 print("Stop words removed and saved to new file successfully!")
